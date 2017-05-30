e4a449ceebcbb5ae5e8ababbb8446c261642cfff
Christophe <christophe@colorfield.eu>
2017-05-30 13:55:04 +0200

    Remove unused Navigation, typo fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,7 +40,6 @@ class Header extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <Navigation />
           <Link className={s.brand} to="/">
             <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
             <span className={s.brandTxt}>
