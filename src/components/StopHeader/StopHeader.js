d6383b1be7ad1667bedd89e1d750e5984de1c189
Christophe <christophe@colorfield.eu>
2017-12-08 09:42:22 +0100

    Add itinerary icon to StopHeader

--- src/components/StopHeader/StopHeader.js
+++ src/components/StopHeader/StopHeader.js
@@ -4,6 +4,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './StopHeader.css';
 import Link from '../Link';
 import LanguageSwitcher from '../LanguageSwitcher';
+import { JSON_API_URL } from '../../constants/env';
 
 class StopHeader extends React.Component {
   static propTypes = {
@@ -29,17 +30,47 @@ class StopHeader extends React.Component {
     }).isRequired,
   };
 
+  getImageFromIncluded(imageId) {
+    let result = null;
+    const image = this.props.itinerary.included.filter(
+      obj => obj.id === imageId,
+    );
+    if (image[0]) {
+      result = `${JSON_API_URL}/${image[0].attributes.url}`;
+    }
+    return result;
+  }
+
+  /**
+   * Attaches the includes Url to the itinerary data.
+   *
+   * @returns {Array}
+   */
+  itineraryWithIncludesUrl() {
+    const itinerary = this.props.itinerary;
+    const tmpItinerary = itinerary.data;
+    if (itinerary.data.relationships.field_image.data !== null) {
+      const iconImageId = itinerary.data.relationships.field_image.data.id;
+      tmpItinerary.iconImageUrl = this.getImageFromIncluded(iconImageId);
+    }
+    return tmpItinerary;
+  }
+
   render() {
-    const { stop, itinerary } = this.props;
+    const stop = this.props.stop;
+    const itinerary = this.itineraryWithIncludesUrl();
 
     return (
       <div className={s.root}>
         <div className={s.container}>
           <Link to={`/itinerary/${this.props.itinerary.data.id}`}>
             Back to itinerary
+            {itinerary.iconImageUrl !== null
+              ? <img src={itinerary.iconImageUrl} alt={itinerary.title} />
+              : <span>Image empty state</span>}
           </Link>
           <span className={s.stopLocation}>
-            {itinerary.data.attributes.name} | {stop.data.attributes.field_id}
+            {itinerary.attributes.name} | {stop.data.attributes.field_id}
           </span>
           <LanguageSwitcher />
           <h1>
