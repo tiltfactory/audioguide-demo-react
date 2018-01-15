701ced19a3d2c21360859330fa026262a3aef227
Christophe <christophe@colorfield.eu>
2018-01-15 11:14:40 +0100

    Action should not throw error in case of a network issue

--- src/routes/itinerary/index.js
+++ src/routes/itinerary/index.js
@@ -7,12 +7,18 @@ async function action({ locale, params }) {
   const drupalLocale = locale.substring(0, 2); // @todo improve
 
   // Fetch the localized itinerary term.
-  // @todo replace throws error
   const itineraryTermEndpoint = `${JSON_API_URL}/${drupalLocale}/jsonapi/taxonomy_term/audio_itinerary/${params.itinerary_id}?_consumer_id=${CONSUMER_ID}&include=field_image,field_background_image`;
-  const itineraryTerm = await fetch(itineraryTermEndpoint).then(response =>
-    response.json(),
-  );
-  if (!itineraryTerm) throw new Error('Failed to load the itinerary.');
+  let itineraryTerm = {};
+  let title = {};
+  try {
+    itineraryTerm = await fetch(itineraryTermEndpoint).then(response =>
+      response.json(),
+    );
+    // Set page name from the current itinerary.
+    title = itineraryTerm.data.attributes.name;
+  } catch (error) {
+    // @todo improve error management with PropTypes
+  }
 
   // Fetch the child itineraries from the current parent itinerary if any.
   // @todo currently, there is an issue on getting the parent / child relationship
@@ -27,17 +33,16 @@ async function action({ locale, params }) {
     // @todo improve error management with PropTypes
   }
 
-  // Set page name from the current itinerary.
-  const title = itineraryTerm.data.attributes.name;
-
   // Fetch the translated node stops for this itinerary.
-  // @todo replace throws error
   const itineraryStopNodesEndpoint = `${JSON_API_URL}/${drupalLocale}/jsonapi/node/audio?_consumer_id=${CONSUMER_ID}&sort=field_weight&filter[field_audio_itinerary.uuid][value]=${params.itinerary_id}&include=field_image`;
-  const itineraryStopNodes = await fetch(
-    itineraryStopNodesEndpoint,
-  ).then(response => response.json());
-  if (!itineraryStopNodes)
-    throw new Error('Failed to load the stops for the itinerary.');
+  let itineraryStopNodes = {};
+  try {
+    itineraryStopNodes = await fetch(
+      itineraryStopNodesEndpoint,
+    ).then(response => response.json());
+  } catch (error) {
+    // @todo improve error management with PropTypes
+  }
 
   // Fetch all the available translated node stops that are not part of the
   // current itinerary.
